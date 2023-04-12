c7d123939427beb9f7b1c93b8a554b12e2bfc8c3
KonradStaniec <konrad.staniec@gmail.com>
2023-04-12 09:16:54 +0200

    add white space

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -3,6 +3,7 @@ mod query;
 
 pub use querier::BabylonQuerier;
 pub use query::{BabylonQuery, CurrentEpochResponse};
+
 // This export is added to all contracts that import this package, signifying that they require
 // "babylon" support on the chain they run on.
 #[no_mangle]
